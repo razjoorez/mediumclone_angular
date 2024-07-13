d3e8fe55221bf900e2af4250728d2538aad46e21
reza razjoo <reza.razjoo@yahoo.com>
2024-07-13 23:58:55 +0100

    sign up form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,24 @@
 import { Component } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
   selector: 'mc-register',
   templateUrl:'./register.component.html',
   standalone: true,
+  imports: [ReactiveFormsModule]
 })
 
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+  username: ['', Validators.required],
+  email: ['', Validators.email],
+  password:['',Validators.required ],
+  })
+  constructor(private fb:FormBuilder) {
+
+  }
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue())
+  }
+}
