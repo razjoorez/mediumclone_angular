24aa3a22ae2c1516a58a74370f8349a69354ac12
reza razjoo <reza.razjoo@yahoo.com>
2024-07-13 23:08:37 +0100

    register comp

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'mc-register',
+  templateUrl:'./register.component.html',
+  standalone: true,
+})
+
+export class RegisterComponent {}
